302ee14abb49f3489a79076fe60ed53597f1c9c7
Elvine7067 <achiengelvine1@gmail.com>
2023-01-23 15:56:11 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 alert("Saying hi from a different file");
+// let a = 10;
+console.log("a");
+prompt("Hi! How are you");
+// Math.random(); -this is the command to create a ranom number
+console.log(Math.random()); 
+// the above code will be a decimal between 0 and 1.We can multiply it by 100 id we want a number between 0 and 100.
+console.log(Math.random() * 100);
